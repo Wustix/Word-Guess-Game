7c9366ac270fadf58a9d24a4dba9bacf02de2d18
unknown <wroehrman@yahoo.com>
2018-05-21 20:59:47 -0500

    updates to word guess files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,16 @@
 var validLetterGuess = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var randomWords = []
+var wins = 0;
+var losses = 0;
+var incorrectGuessLetters = (incorrectGuessLetters)
+var correctGuessLetters = (correctGuessLetters)
+var gamesWon = (gamesWon)
+var gamesLost = (gamesLost)
+var pressAnyKeyToStart =(pressAnyKeyToStart)
+var currentWord = (currentWord)
+var numberOfGuessesRemaining = (numberOfGuessesRemaining)
+var youWin = (youWin)
+var youLose = (youLose)
+var newGame = (newGame)
+
+
