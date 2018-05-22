eccc817a5051f2d85e2e564c0117755ac7afc45d
unknown <wroehrman@yahoo.com>
2018-05-22 18:25:00 -0500

    file update to word games JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var validLetterGuess = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var randomWords = []
+var randomWords = ['beachball', 'chair', 'sand', 'ocean', 'lifeguard', 'sandcastle', 'boats', 'pier', 'towel', 'lotion', 'dolphin', 'drinks', 'seashells', 'crabs', 'swimsuit', 'sunglasses']
 var incorrectGuessLetters = (incorrectGuessLetters)
 var correctGuessLetters = (correctGuessLetters)
 var wins = 0;
@@ -15,3 +15,6 @@ var newGame = (newGame)
 var gameOver = (gameOver)
 
 
+document.onkeyup = function() {
+    
+}
