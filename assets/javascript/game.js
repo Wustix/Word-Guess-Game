b3ef695449c97509bd1a243afa9c2fb75ac157ce
unknown <wroehrman@yahoo.com>
2018-05-23 19:24:13 -0500

    updates to JS file in word guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,29 @@ var validLetterGuess = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n'
 var randomWords = ['beachball', 'chair', 'sand', 'ocean', 'lifeguard', 'sandcastle', 'boats', 'pier', 'towel', 'lotion', 'dolphin', 'drinks', 'seashells', 'crabs', 'swimsuit', 'sunglasses']
 var incorrectGuessLetters = (incorrectGuessLetters)
 var correctGuessLetters = (correctGuessLetters)
+// Var for wins and losses.
 var wins = 0;
 var losses = 0;
-var gamesWon = (gamesWon)
-var gamesLost = (gamesLost)
-var youWin = (youWin)
-var youLose = (youLose)
-var pressAnyKeyToStart =(pressAnyKeyToStart)
+
+
+
 var currentWord = (currentWord)
 var numberOfGuessesRemaining = (numberOfGuessesRemaining)
 var newGame = (newGame)
 var gameOver = (gameOver)
 
-
+// Function runs when a user presses a key.
 document.onkeyup = function(event) {
-    var pressAnyKeyToStart = event.key;
+
+// Determines key that was pressed.   
+    var userBegin = event.key;
+
+// Random word chosen from word array.    
+    var wordChoice = randomWords[Math.floor(Math.random() * randomWords.length)];
+
+    if 
+
+
+
 
 }
