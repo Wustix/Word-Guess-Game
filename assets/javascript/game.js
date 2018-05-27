713a074a3b83230f2b5942d44e47249a1e576839
unknown <wroehrman@yahoo.com>
2018-05-27 18:08:53 -0500

    update to word guess files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var validLetterGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var validLetterGuess = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'];
 var randomWords = ['beachball', 'chair', 'sand', 'ocean', 'lifeguard', 'sandcastle', 'boats', 'pier', 'towel', 'lotion', 'dolphin', 'drinks', 'seashells', 'crabs', 'swimsuit', 'sunglasses'];
 var incorrectGuessLetters = (incorrectGuessLetters);
 var correctGuessLetters = (correctGuessLetters);
@@ -18,7 +18,7 @@ var wrongLetter = [];
 var underScores = [];
 var userGuess = [];
 var wordChoice;
-
+var winCount = 0;
 
 
 // Function runs when user presses a key.
@@ -26,13 +26,56 @@ var wordChoice;
 function startGame() {
 
     wordChoice = randomWords[Math.floor(Math.random() * randomWords.length)];
+    console.log("word Choice = " + wordChoice);
+    for (i = 0; i < wordChoice.length; i++) {
 
-    for (i = 0; i < wordChoice.length; i++)
-{
     underScores.push("_");
 }
+    document.getElementById("word-blanks").textContent = underScores.join(" ");
+
+// Game restart.
+    wrongLetter = [];
+    guessRemaining = 12;
+
+// Page content.
+    document.getElementById("num-left").textContent = guessRemaining;
+}
+function winOrLose() {
+    if (winCount === wordChoice.length);
+
+    else if (guessRemaining === 0); 
+}
+    
+
+// User input.
+document.onkeyup = function(event) {
+
+    userGuess = event.key;
+    
+    if (wordChoice.indexOf(userGuess) > -1) {
+
+            for (var i = 0; i < wordChoice.length; i++)
+            {
+                if(wordChoice[i] === userGuess)
+                {
+                    underScores[i] = userGuess;
+                    console.log(underScores);
+                    winCounter++;
+                    winOrLose();
+                }
+            }
 }
 
+else {
+
+    wrongLetter.push(userGuess);
+    guessRemaining--;
+    console.log(guessRemaining);
+    winOrLose();
+}
+
+
+}
 
 
 startGame();
